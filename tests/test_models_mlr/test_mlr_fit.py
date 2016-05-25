df7af4deec2c1dff76086e2fd48c19c2441f8f21
Robert Walecki <rw2614@imperial.ac.uk>
2016-05-25 10:56:22 +0100

    code cleanup

--- tests/test_models_mlr/test_mlr_fit.py
+++ tests/test_models_mlr/test_mlr_fit.py
@@ -7,36 +7,24 @@ dat = cPickle.load(gzip.open('./tests/data/disfa_slim.pklz','rb'))
 X = np.vstack(dat['X'])
 y = np.vstack(dat['y'])
 
+def clr_fit_predict(clf):
+    y_hat = clf.fit(X,y).predict(X)
+    mse_0 = np.mean((y-np.zeros_like(y))**2,0).mean()
+    mse_p = np.mean((y-y_hat)**2,0).mean()
+    mse_r = np.mean((y-np.random.randint(0,5,y.shape))**2,0).mean()
+    return (mse_0 > mse_p) and (mse_r > mse_p)
+
+
 
 class testcase:
 
-
     def test_mlr(self):
-
-        clf = cor.models.MLR(C=0,max_iter=50)
-        y_hat = clf.fit(X,y).predict(X)
-
-        mse_0 = np.mean((y-np.zeros_like(y))**2,0).mean()
-        mse_p = np.mean((y-y_hat)**2,0).mean()
-        mse_r = np.mean((y-np.random.randint(0,5,y.shape))**2,0).mean()
-
-        assert mse_0 > mse_p
-        assert mse_r > mse_p
-
+        clf = cor.models.MLR(max_iter=50)
+        assert clr_fit_predict(clf)
 
     def test_sor(self):
-
-        clf = cor.models.MLR(C=0,max_iter=50)
-        y_hat = clf.fit(X,y).predict(X)
-
-        mse_0 = np.mean((y-np.zeros_like(y))**2,0).mean()
-        mse_p = np.mean((y-y_hat)**2,0).mean()
-        mse_r = np.mean((y-np.random.randint(0,5,y.shape))**2,0).mean()
-
-        assert mse_0 > mse_p
-        assert mse_r > mse_p
-
-
+        clf = cor.models.SOR(max_iter=50)
+        assert clr_fit_predict(clf)
 
 
 if __name__ == "__main__":
