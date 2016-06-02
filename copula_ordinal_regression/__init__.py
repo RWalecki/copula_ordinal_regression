a556dfbce513762ca58f6665e54e808b7c823ca1
Robert Walecki <rw2614@imperial.ac.uk>
2016-06-02 19:14:45 +0100

    add: fera2015, shoulder_pain

--- copula_ordinal_regression/__init__.py
+++ copula_ordinal_regression/__init__.py
@@ -1,4 +1,4 @@
 from .COR import COR
 import metrics
-from .data_sets import load_disfa, load_fera, load_pain
+from .data_sets import load_disfa, load_fera2015, load_shoulder_pain
 from .tespo import tespo
