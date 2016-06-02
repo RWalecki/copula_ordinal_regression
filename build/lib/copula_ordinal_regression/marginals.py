2b3e45cf301dc1d29b190df1c795f2e874835276
Robert Walecki <rw2614@imperial.ac.uk>
2016-06-02 19:20:28 +0100

    rm: old files

--- build/lib/copula_ordinal_regression/marginals.py
+++
@@ -1,7 +0,0 @@
-import theano.tensor as TT
-
-def sigmoid(nu, sigma, z):
-    return TT.nnet.sigmoid((z-nu)/sigma)
-
-def normcdf(nu, sigma, z):
-    return 0.5 * ( 1 + TT.erf( (z-nu) / (sigma * (2**0.5) ) ) )
--- build/lib/copula_ordinal_regression/tespo/__init__.py
+++
@@ -1,1 +0,0 @@
-from .tespo import tespo
