24e4bc9b775416f1ae06c0c43e52dfdd624477d8
Robert Walecki <rw2614@imperial.ac.uk>
2016-06-04 16:49:27 +0100

    upd: pklz -> npz fomrat

--- copula_ordinal_regression/data_sets.py
+++ copula_ordinal_regression/data_sets.py
@@ -1,28 +1,24 @@
-try:
-    import cPickle
-except ImportError:
-    import pickle as cPickle
 import gzip
 import os
 import numpy as np
 pwd = os.path.join(os.path.dirname(__file__))
 
 def load_disfa():
-    dat = cPickle.load(gzip.open(pwd+'/data/disfa.pklz','rb'))
+    dat = np.load(pwd+'/data/disfa.npz')
     X = np.vstack(dat['X'])
     y = np.vstack(dat['y'])
     S = np.hstack([[ii]*jj.shape[0] for ii,jj in zip(dat['S'],dat['y'])])
     return X,y,S
 
 def load_fera2015():
-    dat = cPickle.load(gzip.open(pwd+'/data/fera2015.pklz','rb'))
+    dat = np.load(pwd+'/data/fera2015.npz')
     X = np.vstack(dat['X'])
     y = np.vstack(dat['y'])
     S = np.hstack([[ii]*jj.shape[0] for ii,jj in zip(dat['S'],dat['y'])])
     return X,y,S
 
 def load_shoulder_pain():
-    dat = cPickle.load(gzip.open(pwd+'/data/shoulder_pain.pklz','rb'))
+    dat = np.load(pwd+'/data/shoulder_pain.npz')
     X = np.vstack(dat['X'])
     y = np.vstack(dat['y'])
     S = np.hstack([[ii]*jj.shape[0] for ii,jj in zip(dat['S'],dat['y'])])
